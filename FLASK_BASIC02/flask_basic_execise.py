dafe9fde941ed1e257de702cda8586bca4735376
UGUIS8020 <shibuyamasahiko@gmail.com>
2023-12-29 17:22:37 +0900

    no message

--- FLASK_BASIC02/flask_basic_execise.py
+++ FLASK_BASIC02/flask_basic_execise.py
@@ -7,16 +7,16 @@ def index():
     return "<h1>Prosuct Page</h1>"
 
 @app.route("/product/<product_id>")
-def productid(<product_id):
-    return "<h1>Product Name:</h1>"
-
-
-          product_list = [
+def product(product_id):
+ 
+    product_list = [
         ["1", "ノートパソコンA", "Core i5", "￥68,500"],
         ["2", "ノートパソコンB", "AMD Ryzen 5", "￥81,300"],
         ["3", "ノートパソコンC", "CeleronN4020", "￥64,300"]
     ]
     for product in product_list:
+        if product_id in product:
+            break
 
 if __name__ == "__main__":
     app.run(debug=True)
