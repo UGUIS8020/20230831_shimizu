c2c8a6fe7ea4843f2a2e50cac2447425314f37a8
UGUIS8020 <shibuyamasahiko@gmail.com>
2024-04-22 00:48:33 +0900

    新越谷

---
+++ flask_app/init_db.py
@@ -0,0 +1,9 @@
+from company_blog import db
+from company_blog.models import User
+
+# db.drop_all()
+db.create_all()
+
+user1 = User(username='admin',email="admin_user@test.com",password="123",administrator="1")
+db.session.add(user1)
+db.session.commit()
